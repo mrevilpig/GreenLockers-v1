6b34c3c02984969d5712541a5c0e5c00fb4d6bbf
EC2 Default User <ec2-user@ip-10-154-147-7.ec2.internal>
2014-07-03 06:21:49 +0000

    added ssl and modified PushAccessInfo to push all list of accesses

--- app/models/access.rb
+++ app/models/access.rb
@@ -6,7 +6,7 @@ class Access < ActiveRecord::Base
   
   def generate_pin
     rand = Random.new
-    pin = rand.rand(999999).to_s 
+    pin = (rand.rand(899999)+100000).to_s 
     self.barcode = nil
     logger.info "Pin:" + pin.to_s # for debugging
     self.pin = Digest::SHA1.hexdigest pin
@@ -33,7 +33,7 @@ class Access < ActiveRecord::Base
   def clear
     self.barcode = nil
     self.pin = nil
-    self.update_request_id = self.box.locker.access_request_id + 1
+    #self.update_request_id = self.box.locker.access_request_id + 1
     if self.save
       self.push_access_info
       return true
@@ -48,10 +48,16 @@ class Access < ActiveRecord::Base
     http = Net::HTTP.new(uri.host, uri.port)
     #request = Net::HTTP::Get.new(uri.request_uri)
     request = Net::HTTP::Post.new(uri.request_uri)
-    barcode = self.barcode.nil? ? nil : self.barcode
-    pin = self.pin.nil? ? nil : self.pin
-    access_info_list = {"box_id" => self.box.name.to_s, "pin" => pin, "barcode" => barcode}
-    params = {"action" => "PushAccessInfo", "sDeviceID" => self.box.locker.name.to_s, 'AccessInfoList' => [access_info_list].to_json, 'uMillsec' => '6000' }
+    access_info_list = []
+    boxes = self.box.locker.boxes
+    boxes.each do |b|
+      a = b.access
+      if a.barcode.nil? == false or a.pin.nil? == false
+        obj = {"box_id" => b.name.to_s, "pin" => a.pin, "barcode" => a.barcode}
+        access_info_list.push obj
+      end
+    end
+    params = {"action" => "PushAccessInfo", "sDeviceID" => self.box.locker.name.to_s, 'AccessInfoList' => access_info_list.to_json, 'uMillsec' => '6000' }
     logger.info params
     request.set_form_data(params)
     #request.basic_auth("username", "password")
